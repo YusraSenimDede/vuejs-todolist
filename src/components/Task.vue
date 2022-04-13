d5a511e1c2a02c9ac4aeb124eb10bcd4b8b4c019
YusraSenimDede <50294199+YusraSenimDede@users.noreply.github.com>
2022-04-14 00:34:58 +0400

    first commit

---
+++ src/components/Task.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <div class="task">
+      <!-- title -->
+      <div class="title">
+        <h1>To Do List</h1>
+      </div>
+      <!-- form -->
+      <div class="form">
+        <input type="text" placeholder="New Task" />
+        <button><i class="fas fa-plus"></i></button>
+      </div>
+      <!-- task lists -->
+      <div class="taskItems">
+        <ul>
+          <li v-for="task in tasks" :key="task.id">
+            <button><i class="far fa-circle"></i>{{task.title}}</button>
+            <button><i class="far fa-trash-alt"></i></button>
+          </li>
+         
+        </ul>
+      </div>
+      <!-- buttons -->
+      <div class="clearBtns">
+        <button @click="clearCompleted">Clear completed</button>
+        <button @click="clearAll">Clear all</button>
+      </div>
+      <!-- pending task -->
+      <div class="pendingTasks">
+        <span>Pending Tasks: {{incomplete}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Task",
+  props:['tasks'],
+  computed:{
+    incomplete(){
+      return this.tasks.filter(this.inProgress).length ;
+
+    },
+  },
+  methods:{
+    inProgress(tasks){
+      return !this.isComplete(task);
+    },
+    isComplete(tasks){
+      return tasks.completed;
+    },
+    clearCompleted(){
+      this.tasks= this.tasks.filter(this.inProgress);
+    },
+
+    clearAll() {
+      this.tasks=[];
+    }
+  },
+};
+</script>
